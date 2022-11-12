fd7c1e5b1d214d97b7cfb3576ab97769ec7c38fb
gantoin <gantoin@pm.me>
2022-11-12 10:40:57 +0100

    Init project

---
+++ src/main/java/fr/gantoin/githubbadge/GithubBadgeApplication.java
@@ -0,0 +1,13 @@
+package fr.gantoin.githubbadge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubBadgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubBadgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/gantoin/githubbadge/GithubBadgeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gantoin.githubbadge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubBadgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
