67869b54846412bfaa0f349f2a6192310bf5efed
gantoin <gantoin@pm.me>
2022-11-12 14:33:09 +0100

    Some tests, work in progress

--- src/main/java/fr/gantoin/githubbadge/Application.java
+++ src/main/java/fr/gantoin/githubbadge/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GithubBadgeApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GithubBadgeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
--- src/test/java/fr/gantoin/githubbadge/GithubBadgeApplicationTests.java
+++ src/test/java/fr/gantoin/githubbadge/GithubBadgeApplicationTests.java
@@ -1,13 +1,87 @@
 package fr.gantoin.githubbadge;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GithubBadgeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    public static void takeSnapShot(WebDriver webdriver, String fileWithPath) throws Exception {
 
+        //Convert web driver object to TakeScreenshot
+
+        TakesScreenshot scrShot = ((TakesScreenshot) webdriver);
+
+        //Call getScreenshotAs method to create image file
+
+        File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+
+        //Move image file to new destination
+
+        File DestFile = new File(fileWithPath);
+
+        //Copy file at destination
+
+        FileUtils.copyFile(SrcFile, DestFile);
+
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+
+    public void testGuru99TakeScreenShot() throws Exception {
+
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        driver = new ChromeDriver();
+
+        //goto url
+        driver.get("http://github.com/gantoin");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,1500)", "");
+
+        // Locate the element on the web page
+        WebElement logo = driver.findElement(By.xpath("//*[@id=\"user-profile-frame\"]/div/div[3]/div/div[1]/div[1]/div[1]/div/div/svg"));
+
+        // Get screenshot of the visible part of the web page
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        // Convert the screenshot into BufferedImage
+        BufferedImage fullScreen = ImageIO.read(screenshot);
+
+        //Find location of the webelement logo on the page
+        Point location = logo.getLocation();
+
+        //Find width and height of the located element logo
+        int width = logo.getSize().getWidth();
+        int height = logo.getSize().getHeight();
+
+        //cropping the full image to get only the logo screenshot
+        BufferedImage logoImage = fullScreen.getSubimage(location.getX(), location.getY(),
+                width, height);
+        ImageIO.write(logoImage, "png", screenshot);
+
+        //Save cropped Image at destination location physically.
+        FileUtils.copyFile(screenshot, new File("/tmp/particularElementScreenshot.PNG"));
+
+        driver.quit();
+
+    }
 }
